54d0b1edddd4eaae8639adfee33ccef765065d50
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2023-04-21 18:46:06 +0100

    Fix service subtitle

--- filters.py
+++ filters.py
@@ -25,19 +25,21 @@ def service_summary(service: Service) -> str:
 
 
 def service_subtitle(service: Service) -> str:
-    c, p = service.celebrant, service.preacher
+    # Feastday (Secondary), Fr XX YY (Preacher: AN Other)
     if service.secondary_feast:
-        s = service.primary_feast.name + ' (' + service.secondary_feast.name + ').'
+        s = service.primary_feast.name + ' (' + service.secondary_feast.name + '), '
     else:
-        s = service.primary_feast.name + '.'
+        s = service.primary_feast.name + ', '
+
+    c, p = service.celebrant, service.preacher
     if c:
         if p and p != c:
-            s += f' {c}. Preacher: {p}.'
+            s += f' {c} (Preacher: {p})'
         else:
-            s += f' {c}.'
+            s += f' {c}'
     else:
         if p:
-            s += f' Preacher: {p}.'
+            s += f' Preacher: {p}'
     return s
 
 
