8ce7b79eaf203fbe7a59d6c07b25af247ec081ba
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2023-09-03 00:51:14 +0100

    Build single executable as well as directory

--- build.bat
+++ build.bat
@@ -1,7 +1,20 @@
-pyinstaller -y^
-               --paths .^
-               --add-data "templates;templates"^
-               --add-data "static;static"^
-               --add-data "data;data"^
-               --icon "static\favicon_io\favicon.ico"^
-               pypew.py
+:: Build directory
+pyinstaller^
+    -y^
+    --paths .^
+    --add-data "templates;templates"^
+    --add-data "static;static"^
+    --add-data "data;data"^
+    --icon "static\favicon_io\favicon.ico"^
+    pypew.py
+
+:: Build single executable
+pyinstaller^
+    -y^
+    --onefile^
+    --paths .^
+    --add-data "templates;templates"^
+    --add-data "static;static"^
+    --add-data "data;data"^
+    --icon "static\favicon_io\favicon.ico"^
+    pypew.py
