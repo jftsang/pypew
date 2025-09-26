6ecf3cfd39113a907be21ca449ae64202ad91aaf
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2025-09-26 15:29:17 +0100

    Rexport csv file
    For #60

---
+++ data/yamls2csv.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pandas as pd
+import yaml
+from pyxtension.streams import stream
+
+feastdir = Path(__file__).parent / 'feasts'
+slugs = (feastdir / '_list.txt').read_text().splitlines()
+feastlist = []
+
+feasts: list[dict] = (
+    stream(slugs)
+    .map(lambda slug: (feastdir / slug).with_suffix('.yaml'))
+    .map(open)
+    .map(yaml.safe_load)
+    .to_list()
+)
+
+df = pd.DataFrame.from_records(feasts)
+fields = "name,month,day,coeaster,coadvent,introit,collect,epistle_ref,epistle,gat,gradual,alleluia,tract,gospel_ref,gospel,offertory,communion".split(
+    ","
+)
+df = df[fields]
+
+df.to_csv(feastdir / "feasts.csv", index=False)
